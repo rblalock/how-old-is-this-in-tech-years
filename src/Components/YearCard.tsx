fe7bea387347e91955f84ca4bb118ede35ec3cc1
Rick Blalock <rblalock@gmail.com>
2021-08-23 22:55:53 -0400

    tweak some things and prep for social image card

---
+++ src/Components/YearCard.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+interface YearCardProps {
+  name?: string | null;
+	year?: string | null;
+	calculatedYear?: number | null;
+}
+
+const YearCard = (props: YearCardProps) => {
+	return (
+		<div className="relative w-94 h-64 bg-cover">
+			<div className="relative flex items-center justify-center w-full h-full">
+				<div className={`space-y-8 shadow-2xl rounded-2xl p-16 opacity-90 bg-gradient-to-br from-red-400 via-red-700 to-red-500 border border-yellow-900`}>
+					<div className="relative">
+						<h1 className="text-4xl font-extrabold text-gray-100 sm:text-5xl">
+							{props.name && props.year ? (
+								<>
+									<span className={`text-yellow-300`}>{props.name}</span> 
+									<br />is
+									<br /><span className={`text-yellow-300 underline`}>{Math.trunc(props.calculatedYear || 0)}</span> tech years old
+								</>
+							) : (
+								<>
+									Want to see how old something is?
+									<br />
+									<a href="/" className={`text-yellow-300 font-bold`}>Go here to try!</a>
+								</>
+							)}
+						</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default YearCard;

0be8b3f7d5acd125f87873dc2271aeafbab344f9
Rick Blalock <rblalock@gmail.com>
2021-08-24 21:46:42 -0400

    footer tweaks

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -13,9 +13,9 @@ const Layout = (props: LayoutProps) => {
 			<section className="text-gray-700 bg-white body-font">
 				<div className="container flex flex-col items-center px-8 py-8 mx-auto max-w-7xl sm:flex-row">
 					<p className="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:mt-0">
-						© 2021 - How old is this in tech years?
+						© How old is this in tech years?
 					</p>
-					<span className="inline-flex justify-center mt-4 sm:ml-auto sm:mt-0 sm:justify-start">
+					<span className="flex flex-wrap sm:inline-flex justify-center mt-4 sm:ml-auto sm:mt-0 sm:justify-start">
 						<span className="text-gray-500 mr-2">
 							Made by:
 						</span>
@@ -29,6 +29,14 @@ const Layout = (props: LayoutProps) => {
 
 						<a href="https://twitter.com/svpino" className="text-red-400 hover:text-red-500 font-semibold">
 							Santiago
+						</a>
+
+						<span className="text-gray-300 mx-1">
+							|
+						</span>
+
+						<a href="https://github.com/rblalock/how-old-is-this-in-tech-years" className="text-gray-400 hover:text-gray-500 font-semibold">
+							Open-Source @ Github
 						</a>
 					</span>
 				</div>
